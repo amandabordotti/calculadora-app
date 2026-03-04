f9d6b07f0fbfb1eeffd55152ae2bab8185840dad
amandabordotti <amandabordotti12@gmail.com>
2026-03-04 12:24:23 -0300

    Continuação

---
+++ calculadora/utils/calcEngine.js
@@ -0,0 +1,53 @@
+import { flixFoat, formatNumber, toNumber } from "./format";
+
+const OPS = new Set(["+", "-", "*", "/"]);
+
+export function createEngine({ locale = "pt-BR"} = {}) {
+  function initialState() {
+    return {
+      display: formatNumber(0, locale),
+      expression: "",
+      storedValue: null,
+      pendingOp: null,
+      isNewEntry: true,
+      lastKey: null,
+    };
+  }
+
+  
+  function reduce(state, key) {
+    const s = { ...state, lastKey: key };
+    // Programar
+    return s;
+  }
+
+  function onDigitOrDot(s, key, locale) {
+
+  }
+
+  function onOperator(s, op, locale) {
+
+  }
+
+  function onEqual(s, locale) {
+
+  }
+
+  function compute(a, b, op) {
+
+  }
+
+  function symbol(op) {
+
+  }
+
+  function inDigit(k) {
+
+  }
+
+  function displayToRaw(displayText) {
+
+  }
+
+  return { initialState, reduce }
+}
